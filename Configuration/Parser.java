91dcb26e7b2d167eb29a4b166f71b26ef31a0ac0
hyunjae1999 <39613298+hyunjae1999@users.noreply.github.com>
2019-02-23 01:10:21 -0500

    Update Parser.java

--- Configuration/Parser.java
+++ Configuration/Parser.java
@@ -12,7 +12,6 @@ import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
 /*
-@author-Hyunjae Lee
 This is an abstract class for a general parser that takes filename, and initializes the document objects needed to parse any xml file.
  */
 public abstract class Parser {
